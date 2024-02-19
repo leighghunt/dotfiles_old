4f6924f6037bc1a55d462bc3ad99bce05686747c
Leigh Hunt <leigh@venari.co.nz>
2024-02-19 21:51:44 +1300

    ctrl-p

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Ctrl-P
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
